38f21902801f64ce4d2441259a4f49b05e2f0578
vicharapu srideepika <deepikavicharapu@gmail.com>
2025-08-03 23:01:40 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,11 @@
+pin=4321
+user=int(input("enter a pin"))
+while(user != pin):
+    print("you enter wrong pin")
+    user=int(input("enter a pin"))
+print("correct pin")
+    
+    
+        
+        
+        
